cde358e3630f6091a1b1fd4e67351afb04c25b90
Yaron Laptop Lenovo <yaron.muzikant@gmail.com>
2017-03-27 15:58:35 +0300

    Scan ports on different threads to optimize port scan duration

--- android/src/main/java/com/ymuzikant/networkscanner/utils/PortScanner.java
+++ android/src/main/java/com/ymuzikant/networkscanner/utils/PortScanner.java
@@ -9,30 +9,53 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Yaron Muzikant on 15-Mar-17.
  */
 
 public class PortScanner {
-    public ArrayList<Integer> scanPorts(String ip, int... ports) {
-        ArrayList<Integer> openPorts = new ArrayList<>();
+    private static final String TAG = "PortScanner";
+
+    public ArrayList<Integer> scanPorts(final String ip, int... ports) {
+        final ArrayList<Integer> openPorts = new ArrayList<>();
 
         if (ports != null && ip != null) {
-            for (int port : ports) {
-                try {
-                    // Try to create the Socket on the given port.
-                    Socket socket = new Socket();
-                    socket.connect(new InetSocketAddress(ip, port), 1000);
+            ThreadPoolExecutor portScanThreads = new ThreadPoolExecutor(ports.length, ports.length, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(ports.length));
 
-                    // If we arrive here, the port is open!
-                    openPorts.add(port);
+            for (final int port : ports) {
+                portScanThreads.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            // Try to create the Socket on the given port.
+                            Socket socket = new Socket();
+                            socket.connect(new InetSocketAddress(ip, port), 1000);
 
-                    // Don't forget to close it
-                    socket.close();
-                } catch (IOException e) {
-                    // Failed to open the port.
-                }
+                            synchronized (openPorts) {
+                                // If we arrive here, the port is open!
+                                openPorts.add(port);
+                            }
+
+                            // Don't forget to close it
+                            socket.close();
+                        } catch (IOException e) {
+                            // Failed to open the port.
+                        }
+                    }
+                });
+            }
+
+            // Wait for all ports scan
+            portScanThreads.shutdown();
+
+            try {
+                portScanThreads.awaitTermination(ports.length * 1000, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
 
