f454fa16b4286a292e7d1f19781b9298852528b8
Yaron Laptop Lenovo <yaron.muzikant@gmail.com>
2017-04-01 19:50:59 +0300

    Add jmdns to NsdDiscovery (Currently runs both Android native NsdDiscovery and jmdns)

--- android/src/main/java/com/ymuzikant/networkscanner/utils/NsdDiscovery.java
+++ android/src/main/java/com/ymuzikant/networkscanner/utils/NsdDiscovery.java
@@ -6,9 +6,17 @@ import android.net.nsd.NsdServiceInfo;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+import javax.jmdns.ServiceTypeListener;
 
 /**
  * Created by Yaron Muzikant on 30-Mar-17.
@@ -17,7 +25,9 @@ import java.util.concurrent.ConcurrentHashMap;
 public class NsdDiscovery {
 
     private static final String TAG = "NsdDiscovery";
-    private NsdManager mNsdManager;
+
+    private JmDNS mJmdns;
+    private final NsdManager mNsdManager;
 
     /*private static String[] SERVICE_TYPES = {
             "_echo._tcp",
@@ -73,26 +83,107 @@ public class NsdDiscovery {
         }
     };
 
+    private final Map<String, ServiceListener> mJmDnsActiveListeners = new ConcurrentHashMap<>();
+    private final Map<String, ServiceInfo> mJmDnsFoundServices = new ConcurrentHashMap<>();
+
+    private ServiceTypeListener mJmDnsServiceTypeListener = new ServiceTypeListener() {
+        @Override
+        public void serviceTypeAdded(ServiceEvent event) {
+            Log.d(TAG, "serviceTypeAdded() called with: event = [" + event + "]");
+
+            if (!mJmDnsActiveListeners.containsKey(event.getType())) {
+                ServiceListener serviceListener = new ServiceListener() {
+                    @Override
+                    public void serviceAdded(ServiceEvent event) {
+                        Log.d(TAG, "serviceAdded() called with: event = [" + event + "]");
+
+                        mJmDnsFoundServices.put(getServiceEventKey(event), event.getInfo());
+                    }
+
+                    @Override
+                    public void serviceRemoved(ServiceEvent event) {
+                        Log.d(TAG, "serviceRemoved() called with: event = [" + event + "]");
+                    }
+
+                    @Override
+                    public void serviceResolved(ServiceEvent event) {
+                        Log.d(TAG, "serviceResolved() called with: event = [" + event + "]");
+
+                        // Overwrite info of same service added in serviceAdded callback
+                        mJmDnsFoundServices.put(getServiceEventKey(event), event.getInfo());
+                    }
+
+                    @NonNull
+                    private String getServiceEventKey(ServiceEvent event) {
+                        return event.getName() + event.getType();
+                    }
+                };
+
+                mJmDnsActiveListeners.put(event.getType(), serviceListener);
+                mJmdns.addServiceListener(event.getType(), serviceListener);
+            }
+        }
+
+        @Override
+        public void subTypeForServiceTypeAdded(ServiceEvent event) {
+            Log.d(TAG, "subTypeForServiceTypeAdded() called with: event = [" + event + "]");
+        }
+    };
+
     private String getServiceFullName(NsdServiceInfo nsdServiceInfo) {
         return nsdServiceInfo.getServiceName() + "." + (nsdServiceInfo.getServiceType().contains("_tcp") ? "_tcp" : "_udp");
     }
 
     public NsdDiscovery(@NonNull Context context) {
         mNsdManager = (NsdManager) context.getSystemService(Context.NSD_SERVICE);
+
+        try {
+            mJmdns = JmDNS.create(InetAddress.getLocalHost());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void startDiscovery() {
         Log.d(TAG, "Start discovery...");
 
+        // Stop active listeners
+        stopNsdDiscoveryListeners();
+        stopJmDnsActiveListeners();
+
         // Find all services published on the network
         mNsdManager.discoverServices("_services._dns-sd._udp", NsdManager.PROTOCOL_DNS_SD, mServiceDiscoveryListener);
+
+        try {
+            mJmdns.addServiceTypeListener(mJmDnsServiceTypeListener);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void stopDiscovery() {
         Log.d(TAG, "Stop discovery...");
 
         mNsdManager.stopServiceDiscovery(mServiceDiscoveryListener);
+        stopNsdDiscoveryListeners();
 
+        mJmdns.removeServiceTypeListener(mJmDnsServiceTypeListener);
+        stopJmDnsActiveListeners();
+    }
+
+    private void stopJmDnsActiveListeners() {
+        synchronized (mJmDnsActiveListeners) {
+            for (Map.Entry<String, ServiceListener> serviceListenerEntry : mJmDnsActiveListeners.entrySet()) {
+                Log.d(TAG, "jmDns stopDiscovery() called for service [" + serviceListenerEntry.getKey() + "]");
+
+                mJmdns.removeServiceListener(serviceListenerEntry.getKey(), serviceListenerEntry.getValue());
+            }
+
+            mJmDnsActiveListeners.clear();
+        }
+    }
+
+    private void stopNsdDiscoveryListeners() {
         synchronized (mActiveListeners) {
             for (NsdManager.DiscoveryListener activeListener : mActiveListeners.values()) {
                 mNsdManager.stopServiceDiscovery(activeListener);
@@ -104,6 +195,10 @@ public class NsdDiscovery {
 
     public Collection<NsdServiceInfo> getFoundServices() {
         return mFoundServices.values();
+    }
+
+    public Collection<ServiceInfo> getFoundServices2() {
+        return mJmDnsFoundServices.values();
     }
 
     private void startServiceDiscovery(NsdServiceInfo nsdServiceInfo) {
